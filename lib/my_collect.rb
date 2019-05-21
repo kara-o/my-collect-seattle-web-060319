5587527f806133d1321e975c621f63382f19024f
Kara O <karajo.odle@gmail.com>
2019-05-21 21:40:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,10 @@ def my_collect(collection)
   i = 0 
   
   while i < collection.length do
-    new_collection << yield(collection[i])
+    yield(collection[i])
     i = i + 1 
   end
 
-new_collection
 
 end
 
